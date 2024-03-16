42f82de10f5f176479b2743d0bbba4cc3ad6e4fc
Mike Dilger <mike@mikedilger.com>
2024-03-16 15:25:12 +1300

    dump_relay_with_login

---
+++ src/bin/dump_relay_with_login.rs
@@ -0,0 +1,31 @@
+use nostr_probe::{Command, Probe};
+use nostr_types::{Filter, RelayMessage};
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = env::args();
+    let _ = args.next(); // program name
+    let relay_url = match args.next() {
+        Some(u) => u,
+        None => panic!("Usage: dump_relay_mike <RelayURL>"),
+    };
+
+    let signer = nostr_probe::load_signer()?;
+
+    let (to_probe, from_main) = tokio::sync::mpsc::channel::<Command>(100);
+    let (to_main, from_probe) = tokio::sync::mpsc::channel::<RelayMessage>(100);
+    let relay_url2 = relay_url.clone();
+    let join_handle = tokio::spawn(async move {
+        let mut probe = Probe::new(from_main, to_main);
+        if let Err(e) = probe.connect_and_listen(&relay_url2).await {
+            eprintln!("{}", e);
+        }
+    });
+
+    let filter = Filter::new();
+
+    nostr_probe::req(&relay_url, signer, filter, to_probe, from_probe).await?;
+
+    Ok(join_handle.await?)
+}
