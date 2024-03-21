658cfe211a527022c1c006a0a8f973d283379438
Mike Dilger <mike@mikedilger.com>
2024-03-21 13:36:35 +1300

    create_event

---
+++ src/bin/create_event.rs
@@ -0,0 +1,24 @@
+use nostr_types::{PreEvent, Signer, Unixtime};
+use std::env;
+use std::io::Read;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = env::args();
+    let _ = args.next(); // program name
+
+    let mut s: String = String::new();
+    std::io::stdin().read_to_string(&mut s)?;
+    let mut pre_event: PreEvent = serde_json::from_str(&s)?;
+
+    // Update creation stamp
+    pre_event.created_at = Unixtime::now().unwrap();
+
+    let signer = nostr_probe::load_signer()?;
+
+    let event = signer.sign_event(pre_event)?;
+
+    println!("{}", serde_json::to_string(&event)?);
+
+    Ok(())
+}
