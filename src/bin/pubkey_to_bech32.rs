ee5774e7ab9322f6263a59523f5d93cde43c5717
Mike Dilger <mike@mikedilger.com>
2024-05-06 09:43:55 +1200

    pubkey_to_bech32 different more obvious interface

--- src/bin/pubkey_to_bech32.rs
+++ src/bin/pubkey_to_bech32.rs
@@ -1,7 +1,14 @@
 use nostr_types::PublicKey;
+use std::env;
 
 fn main() {
-    let hex = rpassword::prompt_password("Public key hex: ").unwrap();
+    let mut args = env::args();
+    let _ = args.next(); // program name
+    let hex = match args.next() {
+        Some(u) => u,
+        None => panic!("Usage: pubkey_to_bech32 <hex>"),
+    };
+
     let public_key = PublicKey::try_from_hex_string(&hex, true).unwrap();
     let bech32 = public_key.as_bech32_string();
     println!("{}", bech32);
