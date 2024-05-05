23c07d9e5eefd32103142e77aeb03960871439c9
Mike Dilger <mike@mikedilger.com>
2024-05-06 10:24:30 +1200

    load_signer (from encrypted private key in $HOME/.config/nostr-probe/epk)

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,12 @@ use futures_util::stream::FusedStream;
 use futures_util::{SinkExt, StreamExt};
 use http::Uri;
 use lazy_static::lazy_static;
-use nostr_types::{ClientMessage, Event, Filter, RelayMessage, SubscriptionId};
+use nostr_types::{
+    ClientMessage, EncryptedPrivateKey, Event, Filter, KeySigner, RelayMessage, Signer,
+    SubscriptionId,
+};
 use tungstenite::Message;
+use zeroize::Zeroize;
 
 type Ws =
     tokio_tungstenite::WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>;
@@ -227,4 +231,32 @@ pub fn url_to_host_and_uri(url: &str) -> (String, Uri) {
         panic!("URL has empty hostname");
     }
     (host.to_owned(), uri)
+}
+
+pub fn load_signer() -> Result<KeySigner, Box<dyn std::error::Error>> {
+    let mut config_dir = match dirs::config_dir() {
+        Some(cd) => cd,
+        None => panic!("No config_dir defined for your operating system"),
+    };
+    config_dir.push("nostr-probe");
+    config_dir.push("epk");
+
+    let epk_bytes = match std::fs::read(&config_dir) {
+        Ok(bytes) => bytes,
+        Err(e) => {
+            eprintln!("{}", e);
+            panic!(
+                "Could not find your encrypted private key in {}",
+                config_dir.display()
+            );
+        }
+    };
+    let epk_string = String::from_utf8(epk_bytes)?;
+    let epk = EncryptedPrivateKey(epk_string);
+
+    let mut password = rpassword::prompt_password("Password: ").unwrap();
+    let mut signer = KeySigner::from_encrypted_private_key(epk, &password)?;
+    signer.unlock(&password)?;
+    password.zeroize();
+    Ok(signer)
 }
