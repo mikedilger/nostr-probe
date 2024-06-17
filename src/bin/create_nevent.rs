2f066789a0e5ffe74b9a9c30696543c8737a14d1
Mike Dilger <mike@mikedilger.com>
2024-06-17 15:18:00 +1200

    create_nevent

---
+++ src/bin/create_nevent.rs
@@ -0,0 +1,30 @@
+use nostr_types::{EventPointer, Id, NostrBech32, UncheckedUrl};
+use std::env;
+
+fn main() {
+    let mut args = env::args();
+    let _ = args.next(); // program name
+
+    let hex = match args.next() {
+        Some(u) => u,
+        None => panic!("Usage: create_nevent <idhex> <relay_url> [<relay_url>]"),
+    };
+    let id = Id::try_from_hex_string(&hex).unwrap();
+
+    let mut relays: Vec<UncheckedUrl> = Vec::new();
+    while let Some(urlstr) = args.next() {
+        let url = UncheckedUrl::from_str(&urlstr);
+        relays.push(url);
+    }
+
+    let ep = EventPointer {
+        id,
+        relays,
+        kind: None,
+        author: None,
+    };
+
+    let nurl = NostrBech32::EventPointer(ep);
+
+    println!("{}", nurl);
+}
